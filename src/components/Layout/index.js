0014a4891b1988aee96dc037936a7af4eedaa732
Lucas Sena <lucassena.rj@gmail.com>
2021-04-27 03:07:45 +0100

    Modifying toolbar size

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,8 +3,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Container from "@material-ui/core/Container";
+import MenuIcon from "@material-ui/icons/Menu";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Drawer } from "./../";
 import { Base } from "./styles";
@@ -16,19 +16,19 @@ function Layout({ children, title }) {
     <Base>
       <CssBaseline />
       <AppBar position="static">
-        <Toolbar>
-          <IconButton onClick={() => setDisplay(!display)} edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            { title }
-          </Typography>
-        </Toolbar>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <IconButton onClick={() => setDisplay(!display)} edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              { title }
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
       <Drawer display={display} handle={() => setDisplay(!display)} />
-      <Container>
-        { children }
-      </Container>
+      { children }
     </Base>
   );
 };
