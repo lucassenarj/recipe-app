3266eb83cfa8974dc251bfbb3718bf7244efee6f
Lucas Sena <lucassena.rj@gmail.com>
2021-04-28 00:49:26 +0100

    Added SmallRecipe component

---
+++ src/components/SmallRecipe/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SmallRecipe() {
+  return (
+    <h4>Small Recipe</h4>
+  );
+};
+
+export default SmallRecipe;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 export { default as  Drawer } from "./Drawer";
 export { default as  ItemList } from "./ItemList";
 export { default as  Layout } from "./Layout";
+export { default as  MainRecipe } from "./MainRecipe";
 export { default as  NavItems } from "./NavItems";
+export { default as  SideBar } from "./SideBar";
+export { default as  SmallRecipe } from "./SmallRecipe";
