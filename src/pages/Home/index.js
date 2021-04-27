805688199f6a1f2baf42e6457247fa8142b26ccf
Lucas Sena <lucassena.rj@gmail.com>
2021-04-28 00:50:07 +0100

    Added components to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,54 @@
-import React from "react";
-import { Layout } from "./../../components";
+import React, { useEffect, useState } from "react";
+import {
+  Layout,
+  MainRecipe,
+  SideBar,
+  SmallRecipe,
+} from "./../../components";
+import { Content } from "./styles";
+import Grid from "@material-ui/core/Grid";
+import api from "./../../services/api";
 
 function Home() {
+  const [values, setValues] = useState({
+    main: {},
+    smalls: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    async function getCategories() {
+      const responses = await api.getMultiples();
+      setValues({
+        main: responses[0],
+        smalls: [
+          responses[1],
+          responses[2],
+        ],
+        loading: false,
+      });
+    }
+
+    getCategories();
+  }, []);
+
+  const { loading, main, smalls } = values;
+
   return (
     <Layout title="Home Page">
-      <h1>Home page</h1>
+      <Content>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} md={3}>
+            <SideBar loading={loading} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <MainRecipe loading={loading} recipe={main} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={3}>
+            <SmallRecipe loading={loading} recipes={smalls} />
+          </Grid>
+        </Grid>
+      </Content>
     </Layout>
   );
 };
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import Container from "@material-ui/core/Container";
+
+export const Content = styled(Container)`
+  margin: 20px auto;
+`;
