b72d3a0dca7dc116be24deaf318847c83ca9c8f7
Lucas Sena <lucassena.rj@gmail.com>
2021-04-30 17:00:23 +0100

    fix: fixed Header navigation
    Removed hamburger button when the screen size is larger than "md" breakpoint

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,43 +1,60 @@
 import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { useTheme, ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { Drawer } from "./../";
+import { Base, Link, MenuButton, NavBar, NavMenu } from "./styles";
 import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import IconButton from "@material-ui/core/IconButton";
-import { Link } from "./styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Hidden from "@material-ui/core/Hidden";
 import MenuIcon from "@material-ui/icons/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import Toolbar from "@material-ui/core/Toolbar";
-import { Drawer } from "./../";
-import { Base, NavMenu } from "./styles";
+import Typography from "@material-ui/core/Typography";
 import { menuItems } from "./../../utils/constants";
 
+
 function Layout({ children, title }) {
+  const theme = useTheme();
   const [display, setDisplay] = useState(false);
 
   return (
-    <Base>
-      <CssBaseline />
-      <AppBar position="static">
-        <Container maxWidth="lg">
-          <Toolbar>
-            <IconButton onClick={() => setDisplay(!display)} edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <NavMenu>
-              {
-                menuItems.map(({ path, title }) => (
-                  <Link key={title} to={path}>
-                    <MenuItem>{ title }</MenuItem>
-                  </Link>
-                ))
-              }
-            </NavMenu>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Drawer display={display} handle={() => setDisplay(!display)} />
-      { children }
-    </Base>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Base>
+          <CssBaseline />
+          <AppBar position="static">
+            <Container maxWidth="lg">
+              <NavBar>
+                <Hidden mdUp implementation="css">
+                  <MenuButton
+                    onClick={() => setDisplay(!display)}
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                  >
+                    <MenuIcon />
+                  </MenuButton>
+                </Hidden>
+                <Typography variant="h6" noWrap>
+                  { title }
+                </Typography>
+                <NavMenu>
+                  {
+                    menuItems.map(({ path, title }) => (
+                      <Link key={title} to={path}>
+                        <MenuItem>{ title }</MenuItem>
+                      </Link>
+                    ))
+                  }
+                </NavMenu>
+              </NavBar>
+            </Container>
+          </AppBar>
+          <Drawer display={display} handle={() => setDisplay(!display)} />
+          { children }
+        </Base>
+      </ThemeProvider>
+    </MuiThemeProvider>
   );
 };
 
--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,14 +1,33 @@
 import styled from "styled-components";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
 import MenuList from "@material-ui/core/MenuList";
 import { NavLink } from "react-router-dom";
 
 export const Base = styled.div`
   flex-grow: 1;
 `
+
+export const NavBar = styled(Toolbar)`
+  ${({ theme }) => `
+    justify-content: space-between;
+
+    ${theme.breakpoints.down("sm")} {
+      justify-content: start;
+    }
+  `}
+`;
+
 export const NavMenu = styled(MenuList)`
-  li {
-    display: inline-block;
-  }
+  ${({ theme }) => `
+    li {
+      display: inline-block;
+    }
+
+    ${theme.breakpoints.down("sm")} {
+      display: none;
+    }
+  `}
 `;
 
 export const Link = styled(NavLink)`
@@ -18,4 +37,12 @@ export const Link = styled(NavLink)`
   &:focus, &:hover, &:visited, &:link, &:active {
       text-decoration: none;
   }
+`;
+
+export const MenuButton = styled(IconButton)`
+  ${({ theme }) => `
+    ${theme.breakpoints.up("md")} {
+      display: none;
+    }
+  `}
 `;
